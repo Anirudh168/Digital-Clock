f78aea39c35eaffb271a06fde081d4e7c69fbe3b
Anirudh168 <priyaanirudh2017@gmail.com>
2024-10-04 16:27:58 +0530

    Files added

---
+++ js/script.js
@@ -0,0 +1,28 @@
+var hour = document.getElementById('hour');
+var minute = document.getElementById('minute');
+var second = document.getElementById('second');
+var ampm =  document.getElementById('ampm');
+function updateTime()
+{
+    let ampm1 = "AM";
+    let h = new Date().getHours();
+    let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
+    h = h<10?"0"+h:h;
+    m = m<10?"0"+m:m;
+    s = s<10?"0"+s:s;
+    hour.innerHTML=h;
+    minute.innerHTML=m;
+    second.innerHTML=s;
+    ampm.innerHTML=ampm1;
+    if(h>12)
+    {
+        // h = h-12;
+        ampm1 = "PM";
+        ampm.innerHTML=ampm1;
+    }
+    setTimeout(()=>{
+        updateTime()
+    },1000);
+}
+updateTime();
